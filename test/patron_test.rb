1cf02b46a977ede673f6a042a2105bff381ba024
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 09:15:32 -0600

    Add attribute interests for patron.

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -11,6 +11,7 @@ class PatronTest < Minitest::Test
     assert_instance_of Patron, patron_1
     assert_equal "Bob", patron_1.name
     assert_equal 20, patron_1.spending_money
+    assert_equal [], patron_1.interests
   end
 
 
