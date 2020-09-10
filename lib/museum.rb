4c542cc7509ebd6ed4ffc99102efd74c36cb680f
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 10:16:26 -0600

    Create method recommend exhibits.

--- lib/museum.rb
+++ lib/museum.rb
@@ -8,4 +8,16 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommended_exhibits = []
+    patron.interests.each do |interest|
+      @exhibits.find_all do |exhibit|
+        if exhibit.name == interest
+          recommended_exhibits << exhibit
+        end
+      end
+    end
+    recommended_exhibits
+  end
 end
