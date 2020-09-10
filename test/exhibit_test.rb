8d23a2504e29581689a1de09ddf27227dbdc0310
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 09:10:45 -0600

    Add tests that patron and exhibit classes exist and have attributes.

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -5,6 +5,13 @@ require './lib/exhibit'
 require './lib/patron'
 
 class ExhibitTest < Minitest::Test
+  def test_it_exists_and_has_attributes
+    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    assert_instance_of Exhibit, exhibit
+    assert_equal "Gems and Minerals", exhibit.name
+    assert_equal 0, exhibit.cost
+  end
 
 
 end
